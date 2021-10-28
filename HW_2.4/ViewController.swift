f5fcebfc4647721e538d9c6c30f53b64d3d96ec4
Egor Kromin <egorcrom@mail.ru>
2021-10-29 02:41:33 +0300

    Initial Commit

---
+++ HW_2.4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_2.4
+//
+//  Created by Егор Кромин on 29.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
